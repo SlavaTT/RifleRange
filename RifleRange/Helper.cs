a8b1d9618c44c9645b9f8173cbaf371629bd1be3
Slava Tihonyuk <slava_th@yahoo.com>
2017-11-26 12:05:31 +0200

    On branch master Changes to be committed: new file:   Helper.cs new file:   Web.config new file:   packages.config

---
+++ RifleRange/Helper.cs
@@ -0,0 +1,12 @@
+﻿namespace RifleRange
+{
+    public static class Helper
+    {
+        public static string GetFileNameWithOutGUID(string fileName)
+        {
+            if (!string.IsNullOrEmpty(fileName) && fileName.IndexOf("_") > -1) fileName = fileName.Substring(fileName.IndexOf("_") + 1);
+
+            return fileName;
+        }
+    }
+}
